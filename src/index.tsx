24a83952f055f034243fd3cf5523ab8ecfa29923
mafu <mafusukee@gmail.com>
2023-06-09 23:28:26 +0900

    Release 1.0.1: also apply to reply message

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import manifest, {name as plugin_name} from '../manifest.json'
 import Settings from "./components/Settings"
 import {getByProps} from "enmity/modules"
 import {get} from "enmity/api/settings"
+import {hookAllEvents} from "../../hook";
+import {getByKeyword} from "enmity/metro";
 
 const Patcher = create('ShowUserName')
 const ReactNative = getByProps("View")
@@ -15,15 +17,23 @@ const {DCDChatManager} = ReactNative.NativeModules
 const ShowUserName: Plugin = {
     ...manifest,
     onStart() {
+        function parseMessage(message) {
+            let user = UserStore.getUser(message.authorId)
+            if (!user) return message
+            let username = user.discriminator === "0" ? user.username : user.tag
+            if (user.globalName && get(plugin_name, "display", true)) username += ` - ${user.globalName}`
+            get(plugin_name, "replace", false) ? message.username = username : message.username += ` (${username})`
+            return message
+        }
+
         Patcher.before(DCDChatManager, "updateRows", (_, args, __) => {
             const rows = JSON.parse(args[1])
             for (const row of rows) {
                 if (row.type !== 1 || !row.message?.authorId || !row.message?.username) continue
-                let user = UserStore.getUser(row.message.authorId)
-                if (!user) continue
-                let username = user.discriminator === "0" ? user.username : user.tag
-                if (user.globalName && get(plugin_name, "display", true)) username += ` - ${user.globalName}`
-                get(plugin_name, "replace", false) ? row.message.username = username : row.message.username += ` (${username})`
+                row.message = parseMessage(row.message)
+                if (row.message.referencedMessage?.message) {
+                    row.message.referencedMessage.message = parseMessage(row.message.referencedMessage.message)
+                }
             }
             args[1] = JSON.stringify(rows)
         })
